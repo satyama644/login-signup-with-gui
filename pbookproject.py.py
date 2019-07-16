670f9af26cf9c6192cb8880b0e00541146841aa0
jmsingh6871 <43915113+jmsingh6871@users.noreply.github.com>
2019-07-16 23:54:51 +0530

    Add files via upload
    in this file i add image on background ,ie logo of my BUDDING PROGRAMMER

--- pbookproject.py.py
+++ pbookproject.py.py
@@ -1,17 +1,21 @@
-from tkinter.ttk import *
-from tkinter.ttk import Button as bt
+import tkinter
 import smtplib
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders
+from tkinter.filedialog import askopenfilename
 
 
 def quitt():
     p.destroy()
 
 
-def sign():
+def sign():    # yaha se hum signup start karte h or jo bhi user apni details enter karte h use file me store karate h
     def quit2():
         win.destroy()
     
-    def signup():
+    def signup():#from here we take the input from user
         u=e3.get()
         p=e4.get()
         rp=e5.get()
@@ -33,14 +37,22 @@ def sign():
             signupwin.title('signupwin')
             l10=Label(signupwin,text='thankyou {} for register \nsignup complete'.format(e3.get()))
             l10.place(x=5,y=10)
+
         else:
             signupwin=Tk()
             signupwin.title('signupwin')
             l11=Label(signupwin,text='Retype password do not match')
             l11.place(x=5,y=10)
+    
 
-    win=Tk()
+
+
+    #create a gui window to ask a user to enter the details 
+    win=Toplevel()
     win.geometry('500x500')
+    photo1 = PhotoImage(file=pt)
+    w1 = Label(win, image=photo1)
+    w1.pack()
     l4=Label(win,text='New User signup ',bg='yellow',fg='blue',font=('Edwardian Script ITC',25,'underline'))
     l4.place(x=80,y=10)
     l5=Label(win,text='Username : ',font=('Lucida Calligraphy',8,'bold'))
@@ -65,12 +77,40 @@ def sign():
     e7.place(x=110,y=240)
     b2=Button(win,text='signup',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Blue',command=signup)
     b2.place(x=95,y=280)
-    b3=Button(win,text='Back',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Red',command=quit2)
+    b3=Button(win,text='Back',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Red',command=win.destroy)
     b3.place(x=170,y=280)
-    
+    win.mainloop()
+
+
+
+
 def login():
+    def attachm():
+        global fname
+        fname=askopenfilename()
+        line=fname
+        value=line.split('/')
+        global filename
+        filename=value[-1]
+        lnm6=Label(nmsg,text=filename,font=('Lucida Calligraphy',8,'bold'))
+        lnm6.place(x=60,y=350)
+    
     def nmssg():
         global lnme3
+        mssg=MIMEMultipart()
+        to=lnme3.get()
+        mssg['From']=e
+        mssg['To']=lnme3.get()
+        mssg['Subject']=lnme4.get()
+        body=lnmt5.get('1.0',END)
+        mssg.attach(MIMEText(body,'plain'))
+        attachment=open(fname,"rb")
+        p=MIMEBase('application','octet-stream')
+        p.set_payload((attachment).read())
+        encoders.encode_base64(p)
+        p.add_header('Content-Disposition',"attachment; filename= %s" % filename)
+        mssg.attach(p)
+        text = mssg.as_string()
         # creates SMTP session 
         s = smtplib.SMTP('smtp.gmail.com',587)
         # start TLS for security : it encrypt all the smtp  command so that it will not save our details
@@ -82,7 +122,7 @@ def login():
         to=lnme3.get()
         s.login(e,ep1)
         # sending the mail
-        s.sendmail(e,to,message )
+        s.sendmail(e,to,text)
         #terminating the session
         s.quit()
         print('message send')
@@ -90,10 +130,7 @@ def login():
 
   
 
-    def quit1():
-        readdata.destroy()
-
-    
+        
     def newmssg():
         global to
         global lnme3
@@ -102,10 +139,12 @@ def login():
         global message
         global lnme4
         global lnmt5
-        
+        global nmsg
         nmsg=Toplevel()
         nmsg.geometry('500x500')
-       
+        photo1 = PhotoImage(file=pt)
+        w1 = Label(nmsg, image=photo1)
+        w1.pack()
         lnm1=Label(nmsg,text='New message',bg='yellow',fg='blue',font=('Edwardian Script ITC',25,'underline'))
         lnm1.place(x=120,y=5)
         lnm3=Label(nmsg,text='From : ',font=('Lucida Calligraphy',8,'bold'))
@@ -117,8 +156,6 @@ def login():
         lnm3.place(x=2,y=110)
         lnme3=Entry(nmsg,width=40)
         lnme3.place(x=80,y=110)
-        
-        
         lnm4=Label(nmsg,text='Sub : ',font=('Lucida Calligraphy',8,'bold'))
         lnm4.place(x=2,y=140)
         lnme4=Entry(nmsg,width=40)
@@ -127,15 +164,15 @@ def login():
         lnm5.place(x=2,y=170)
         lnmt5=Text(nmsg,width=40,height=10)
         lnmt5.place(x=80,y=170)
-       
-        
-       
-       
-        blnm1=Button(nmsg,text='attach',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Blue')
-        blnm1.place(x=60,y=350)
-        blnm2=Button(nmsg,text='send',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Blue',command=nmssg)
-        blnm2.place(x=150,y=350)
+        blnm1=Button(nmsg,text='Attach',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Blue',command=attachm)
+        blnm1.place(x=60,y=390)
+        blnm2=Button(nmsg,text='Send',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Blue',command=nmssg)
+        blnm2.place(x=150,y=390)
+        blnm3=Button(nmsg,text='Back',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Red',command=nmsg.destroy)
+        blnm3.place(x=230,y=390)
         nmsg.mainloop()
+
+
     
     def read():
         u=e1.get()
@@ -153,8 +190,11 @@ def login():
             ep1=ep[0:lc]
             
             if u==un and p==ps:
-                loginwin=Tk()
-                loginwin.geometry('400x400')
+                loginwin=Toplevel()
+                loginwin.geometry('500x500')
+                photo1 = PhotoImage(file=pt)
+                w1 = Label(loginwin, image=photo1)
+                w1.pack()
                 loginwin.title('loginwindow')
                 llogin=Label(loginwin,text='•••')
                 llogin.place(x=2,y=5)
@@ -164,17 +204,26 @@ def login():
                 butlogin1.place(x=2,y=65)
                 butlogin2=Button(loginwin,text='Inbox',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Blue')
                 butlogin2.place(x=2,y=95)
+                butlogin3=Button(loginwin,text='Logout',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Red',command=loginwin.destroy)
+                butlogin3.place(x=2,y=125)
+                loginwin.mainloop()
                 break
         else:
-            loginwin=Tk()
+            loginwin=Toplevel()
+            photo1 = PhotoImage(file=pt)
+            w1 = Label(loginwin, image=photo1)
+            w1.pack()
             loginwin.title('loginwindow')
             l9=Label(loginwin,text=('login failed'))
             l9.place(x=5,y=10)
             loginwin.mainloop()
 
 
-    readdata=Tk()
+    readdata=Toplevel()
     readdata.geometry('500x500')
+    photo1 = PhotoImage(file=pt)
+    w1 = Label(readdata, image=photo1)
+    w1.pack()
     ll=Label(readdata,text='Login',bg='yellow',fg='blue',font=('Edwardian Script ITC',25,'underline'))
     ll.place(x=120,y=5)
     l2=Label(readdata,text='Username',font=('Lucida Calligraphy',8,'bold'))
@@ -187,13 +236,18 @@ def login():
     e2.place(x=90,y=120)
     b1=Button(readdata,text='login',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Blue',command=read)
     b1.place(x=90,y=160)
-    b2=Button(readdata,text='Back',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Red',command=quit1)
+    b2=Button(readdata,text='Back',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Red',command=readdata.destroy)
     b2.place(x=160,y=160)
     readdata.mainloop()
 
 from tkinter import *
 p=Tk()
 p.geometry('500x500')
+global pt
+pt='C:\desktop\IMG-20190715-WA0021 (1).png'
+photo = PhotoImage(file=pt)
+imagee = Label(p, image=photo)
+imagee.pack()
 l1=Label(p,text='welcome to python book',bg='yellow',fg='blue',font=('Edwardian Script ITC',25,'underline'))
 l1.place(x=5,y=10)
 but1=Button(p,text='Already user ',font=('Lucida Calligraphy',10,'bold'),fg='White',bg='Blue',command=login)
@@ -202,7 +256,5 @@ but2=Button(p,text='sign up',font=('Lucida Calligraphy',10,'bold'),fg='White',bg
 but2.place(x=4,y=110)
 but3=Button(p,text='quit',bg='Red',fg='White',font=('Lucida Calligraphy',10,'bold'),command=quitt)
 but3.place(x=4,y=140)
-
-
 p.mainloop()
 
